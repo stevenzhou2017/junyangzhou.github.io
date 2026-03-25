e1c41c58f54f44d8abf02ced6693dc8dc822d9cd
Zhou Junyang <junyang.zhou@gmail.com>
2026-03-25 15:36:02 +0800

    Delete password.pl

--- password.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/perl
-
-$fullpath="./";
-$passwd = "2006IS";
-$cpasswd = crypt($passwd, YL);
-
-open (errorlog1, ">>$fullpath/output.txt") or &error("unable to write to output.txt");
-print errorlog1 "password: $passwd | encrypted password: $cpasswd\n\n";
-close(errorlog1);
